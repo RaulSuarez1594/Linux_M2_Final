9e9bf81477b4b107c225485f64cf2554ee5d36eb
jaret <jaretsin@hotmail.com>
2022-10-11 12:10:05 -0500

    Added pseudo_shell function

--- lib/pseudo_shell.c
+++ lib/pseudo_shell.c
@@ -23,3 +23,67 @@ int spawn (char* program, char** arg_list)
 		abort ();
 	}
 }
+// ----- Principal function -----
+int pseudo_shell (int argc, char *argv[])
+{	
+	char s;//-> Variable used to store a character to identify the action to execute.
+// ----- Comparison of the entered argument with the available options -----
+	if(strcmp("fecha",argv[1]) == 0)
+		s = 'f';
+	else if (strcmp("quiensoy",argv[1]) == 0)
+		s = 'q';
+	else if (strcmp("tiempo",argv[1]) == 0)
+		s = 't';
+	else if (strcmp("personal",argv[1]) == 0)
+		s = 'p';
+	else 
+		s = 'e';
+// ----- Detection of the selected option and sends the necessary parameters to the "" function -----
+	switch(s)
+	{
+		case 'f':
+			{
+				char* arg_list[] = {
+					"date",
+					NULL
+					};
+				
+				spawn("date", arg_list);
+				break;
+			}
+		case 'q':
+			{
+				char* arg_list[] = {
+					"whoami",
+					NULL
+				};	
+				spawn("whoami", arg_list);
+				break;
+			}
+		case 't':
+			{
+				char* arg_list[] = {
+					"uptime",
+					"-p",
+					NULL
+				};	
+				spawn("uptime", arg_list);
+				break;
+			}
+		case 'p':
+			{
+				char* arg_list[] = {
+					"/home/jaret/Desktop/Moodle/1_Sistemas_de_archivos/Informacion_y_manipulacion_de_archivos/Jaret",
+					"/home/jaret/Desktop/prueba",
+					NULL
+				};	
+				spawn("/home/jaret/Desktop/Moodle/1_Sistemas_de_archivos/Informacion_y_manipulacion_de_archivos/Jaret", arg_list);
+				break;
+			}
+		default:
+			printf("Option not available\n");
+			break;
+	}
+	wait(NULL);
+	return 0;
+}
