a286f419cee6119987baf9f97bd8e852f2a9105d
ChrisR1999 <christopheradan9@gmail.com>
2022-10-11 14:41:47 -0500

    reformatted code, modification in header file

---
+++ filestats/fileStats.h
@@ -0,0 +1,7 @@
+int printFileType(struct stat stats);
+void printFileSize(struct stat stats);
+void printFileAccess(struct stat stats);
+void printFileInode(struct stat stats);
+void printFileCreateDate(struct stat stats);
+void printFilePropietary(struct stat stats);
+void printFileCharacterCount(char *file_);
