8515b2469cb45f95081eb8101cabd35f7322fc4a
Bryan Casillas Diaz <bryacasillas@gmail.com>
2022-10-11 14:09:22 -0500

    Fixed function errors implementation

--- fileStats.c
+++ fileStats.c
@@ -17,11 +17,23 @@ void printFileSize(struct stat stats)
 }
 
 void printFileAccess(struct stat stats)
-{   
+{
+    printf("\nFile Permissions: ");
+    printf( (S_ISDIR(stats.st_mode)) ? "d" : "-");
+    printf( (stats.st_mode & S_IRUSR) ? "r" : "-");
+    printf( (stats.st_mode & S_IWUSR) ? "w" : "-");
+    printf( (stats.st_mode & S_IXUSR) ? "x" : "-");
+    printf( (stats.st_mode & S_IRGRP) ? "r" : "-");
+    printf( (stats.st_mode & S_IWGRP) ? "w" : "-");
+    printf( (stats.st_mode & S_IXGRP) ? "x" : "-");
+    printf( (stats.st_mode & S_IROTH) ? "r" : "-");
+    printf( (stats.st_mode & S_IWOTH) ? "w" : "-");
+    printf( (stats.st_mode & S_IXOTH) ? "x" : "-");
 }
 
 void printFileInode(struct stat stats)
 {
+    printf("\nInode: %d", stats.st_ino);
 }
 
 void printFileCreateDate(struct stat stats)
@@ -33,33 +45,10 @@ void printFileCreateDate(struct stat stats)
 
 void printFilePropietary(struct stat stats)
 {
-    struct passwd pws;
-    pws = *getpwuid(stats.st_uid);
-    printf("\nPropietary: %s, User UID %d ", pws.pw_name, stats.st_uid);
 }
 
 void printFileCharacterCount(char *file_)
 {
-    char ch;
-    FILE *file;
-    int characters = 0;
-    file = fopen(file_, "r");
-    if (file == NULL)
-    {
-        printf("\nCannot open file %s", file_);
-    }
-
-    while (!feof(file))
-    {
-        ch = fgetc(file);
-        if (isalpha(ch) || isdigit(ch))
-        {
-            characters++;
-        }
-    }
-
-    printf("\nThe alphanumeric count is : %i ", characters);
-    fclose(file);
 }
 
 
