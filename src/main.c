82ccb590fe30454218376bef89f1ae1c7c583b6d
mdeharo <mariano.deharo7571@gmail.com>
2022-10-11 21:46:07 -0500

    Created menu() function

--- src/main.c
+++ src/main.c
@@ -1,33 +1,55 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "include/pseudo_shell.h"
 #include "include/threads_race_condition.h"
 
-int main(int argc, char** argv)
-{
-    int num=0;
-    printf("Que programa deseas ejecutar?\n");
-    printf("5. Pseudo_shell\n");
-    printf("6. Threads_race_condition\n");
-    scanf("%d", &num);
+void menu(void);
 
-    switch(num)
-    {
-        case 5:
-            char* arg;
-            printf("Pseudo-command?\n");
+enum{
+	PSEUDO_SHELL = 5,
+	THREADS_RACE_CONDITION
+};
+
+int gOption = 0;
+
+int main(int argc, char **argv)
+{
+	//do
+	//{
+		menu();
+	//} while (gOption != 3);
+
+	return EXIT_SUCCESS;
+}
+
+
+void menu(void)
+{
+	printf("\t *****************************************\n");
+	printf("\t ------		Main menu	------\n");
+	printf("\t *****************************************\n");
+	printf("\t Which program would you like to execute\?\n");
+	printf("\t Select an option\n");
+	printf("\t [5] pseudo shell     [6] threads race condition      [3] exit\n");
+	scanf("%d",&gOption);
+	system("clear");
+
+	switch(gOption){
+		case PSEUDO_SHELL:
+			printf("Pseudo-command?\n");
             printf("fecha, quiensoy, tiempo, personal\n");
             scanf("%s", arg);
             pseudo_shell(2,arg);
-        break;
-        case 6:
-            threads_race_condition();
-        break;
-        default:
+		break;
+
+		case THREADS_RACE_CONDITION:
+			threads_race_condition();
+		break;
+
+		default:
             printf("Invalid option\n");
-        break;
-    }
-    
-    return 0;
+	}
+
+
 }
