26d7788d994d3f3189a3006432578139634c4be4
ArielGP <ariel.gonzalezp@gmail.com>
2022-10-13 08:45:53 -0700

    Team 2 basefiles re-commited without other files to avoid future conflics

---
+++ src/include/memory_leaks.h
@@ -0,0 +1,4 @@
+
+
+// Funcion para acceder a mem_leak desde main.
+int memory_leaks(void);
---
+++ src/lib/memory_map.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+int var3 = 0;
+
+void foo2(void) {
+    printf("I've just printfed \n");
+}
+
+void foo(int var4) {
+    static int var1 = 0;
+    int var2 = 0;
+    int* var5;
+    void (*var6)(void) = &foo2;
+    var1++;
+    var2++;
+    var3++;
+    var4++;
+    
+    var5 = (int*)malloc(sizeof(int));
+    
+    (*var5)++;
+    
+    printf("var1: %d at %p\n",var1, &var1);
+    printf("var2: %d at %p\n",var2, &var2);
+    printf("var3: %d at %p\n",var3, &var3);
+    printf("var4: %d at %p\n",var4, &var4);
+    printf("var5: %d at %p\n",*var5, var5);
+    printf("var6: - at %p\n", var6);
+    
+}
+
+int bar() 
+{
+    return bar();
+}
+
+int memory_map()
+{
+    for (int i = 0 ; i < 5; i++)
+        foo(0);
+        bar();
+        
+    return 0;
+}
